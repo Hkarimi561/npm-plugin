f3198767c23e5a01c1527a0df1cf62056c8c7a6d
GitHub Action <action@github.com>
2024-12-28 06:47:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/hkarimi561/npmplugin/MyBundle.kt
+++ src/main/kotlin/com/github/hkarimi561/npmplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hkarimi561.npmplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hkarimi561/npmplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/hkarimi561/npmplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.hkarimi561.npmplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
